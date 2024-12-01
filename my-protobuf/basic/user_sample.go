049fbcdd131009cc4e107478d559b13cfbf6502b
gc2321 <gchan.email@gmail.com>
2024-12-01 11:12:20 -0500

    update

--- my-protobuf/basic/user_sample.go
+++ my-protobuf/basic/user_sample.go
@@ -3,6 +3,8 @@ package basic
 import (
 	"log"
 	"my-protobuf/protogen/basic"
+
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func BasicUser() {
@@ -11,7 +13,35 @@ func BasicUser() {
 		Username: "supername",
 		IsActive: true,
 		Password: []byte("supermanpassword"),
+		Emails:   []string{"superman@example.com", "superman@dc.com"},
+		Gender:   basic.Gender_GENDER_MALE,
 	}
 
-	log.Println(&u)
+	jsonBytes, _ := protojson.Marshal(&u)
+	log.Println(string(jsonBytes))
+}
+
+func ProtoToJsonUser() {
+	p := basic.User{
+		Id:       98,
+		Username: "wonderwoman",
+		IsActive: true,
+		Password: []byte("wonderwomanpassword"),
+		Emails:   []string{"wonderwoman@example.com", "wm@dc.com"},
+		Gender:   basic.Gender_GENDER_FEMALE,
+	}
+	jsonBytes, _ := protojson.Marshal(&p)
+	log.Println(string(jsonBytes))
+}
+
+func JsonToProtoUser() {
+	json := `{"id":98,"username":"wonderwoman","is_active":true,"password":"d29uZGVyX21hbl9wd2Q=","emails":["wonderwoman@example.com","wm@dc.com"],"gender":"GENDER_FEMALE"}`
+
+	var p basic.User
+
+	err := protojson.Unmarshal([]byte(json), &p)
+	if err != nil {
+		log.Fatalln("Got error : ", err)
+	}
+	log.Println(&p)
 }
